76441911b1cdeb460eb2b8c2f4084a88a7c4ad66
Krunal12345-ddu <21ceuon099@ddu.ac.in>
2024-12-21 12:09:57 +0530

    Logo changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
         <div className="navbar-left">
-            <Link to="/" className="logo">MastersGang</Link>
+            <Link to="/" className="logo">MentorConnect</Link>
             <input type="text" placeholder="Search classrooms" className="search-input" />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
